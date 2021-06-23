12d0f3c1950e06e99c3e53801e7201d648ad011e
Mateo Perez <mateoperez0508@gmail.com>
2021-06-23 10:32:56 -0500

    enviroment variables

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// @ts-ignore
 import Binance from 'node-binance-api'
 require('dotenv').config()
 const binance = new Binance().options({
   APIKEY: process.env.APIKEY,
   APISECRET: process.env.APISECRET,
 })
-
-export default async (req, res) => {
+type Data = {
+  hola: string
+}
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   let hola = await binance.prices()
 
   res.status(200).json(hola)
